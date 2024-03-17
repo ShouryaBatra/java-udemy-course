1a73d75d27ee0a685199fac17505f4e6e55b76a7
bublibub <shourya.batra@gmail.com>
2024-03-16 19:15:52 -0700

    finished do while

--- Tutorial8-DoWhile/src/DoWhile.java
+++ Tutorial8-DoWhile/src/DoWhile.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
 
 public class DoWhile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello your coputer has virus");
+		
+		Scanner scanner = new Scanner(System.in);
+		/*
+		System.out.println("Enter a nubmer: ");
+		int value = scanner.nextInt();
+		
+		while(value != 5) {
+			System.out.println("Enter a nubmer: ");
+			value = scanner.nextInt();
+		}
+		*/
+		int value = 0;
+		
+		do {
+			System.out.println("enter a number: ");
+			value = scanner.nextInt();
+		}
+		while(value != 5);
+		System.out.println("you got it");
+	
+		
 	}
 
 }
-www
