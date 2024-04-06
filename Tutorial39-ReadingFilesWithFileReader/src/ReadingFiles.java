ab0c60d131cbeeff6dad10e6f7809b4c9bca08e3
ShouryaBatra <shourya.batra@gmail.com>
2024-04-06 10:15:04 -0700

    started arrays adding assignment, almost done

--- Tutorial39-ReadingFilesWithFileReader/src/ReadingFiles.java
+++ Tutorial39-ReadingFilesWithFileReader/src/ReadingFiles.java
@@ -16,9 +16,81 @@ public class ReadingFiles {
 			
 			String line;
 			
-			line = bufferedReader.readLine();
+			String[] bothNumbers = new String[5];
 			
-			System.out.println(line);
+//			while ( (line = bufferedReader.readLine()) != null ) {
+//				System.out.println(line);
+//				
+//			}
+			
+			for (int i=0; (line = bufferedReader.readLine()) != null; i++) {
+				
+				bothNumbers[i] = line;
+				
+			}
+			
+			String[] firstNumber = new String[10];
+			String[] secondNumber = new String[10];
+
+			for (int i=0; i < bothNumbers[0].length(); i++) {
+				firstNumber[i] = bothNumbers[0].charAt(i) + "";
+				
+			}
+			
+			
+			for (int i=0; i < bothNumbers[1].length(); i++) {
+				secondNumber[i] = bothNumbers[1].charAt(i) + "";
+				
+			}
+			
+			
+			
+			
+//			for (int i=0; i<firstNumber.length; i++) {
+//				System.out.print(firstNumber[i] + " ");
+//				
+//			}
+//			
+//			System.out.println();
+//			for (int i=0; i<secondNumber.length; i++) {
+//				System.out.print(secondNumber[i] + " ");
+//			}
+			
+			int[] newNumber = new int[11];
+			
+//			boolean carryOver = false;
+//			
+//			for (int i=10; i<newNumber.length; i = i - 1) {
+//				if ((Integer.parseInt(firstNumber[i]) + Integer.parseInt(firstNumber[i])) >= 9) {
+//					carryOver = true;
+//				}
+//				
+//				if (carryOver = true) {
+//					
+//				}
+//			}
+			
+			int[] firstNumberDigits = new int[20];
+			int[] secondNumberDigits = new int[20];
+			
+			for (int i=0; i<firstNumber.length; i++) {
+				firstNumberDigits[i] = Integer.parseInt(firstNumber[i]);
+				secondNumberDigits[i] = Integer.parseInt(secondNumber[i]);
+			}
+			
+			
+			
+			
+			for (int i=9; i>=0; i = i-1) {
+				newNumber[i] = firstNumberDigits[i] + secondNumberDigits[i];
+
+			}
+			
+			for (int i = 0; i<10; i = i + 1) {
+				System.out.print(newNumber[i]);
+			}
+			
+			
 			
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found: " + file);
