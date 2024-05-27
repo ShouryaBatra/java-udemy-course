493960de32c1e054dc1064ac0f5be4eed368ff0a
ShouryaBatra <shourya.batra@gmail.com>
2024-05-27 11:44:52 -0700

    finished reseto

--- Reseto/src/App.java
+++ Reseto/src/App.java
@@ -14,20 +14,58 @@ public class App {
 			values[i-2] = i;
 		}
 		
-//		for (int i=0; i < values.length; i++) {
-//			System.out.println(values[i]);
+
+		int counter = 0;
+		
+//		for (int i=0; i<values.length; i++) {
+//			if (values[i] != 0) {
+//				values[i] = 0;
+//				counter = counter + 1;
+//				if (counter == K) {
+//					System.out.println(values[i]);
+//					break;
+//				}
+//				int smallestNumber = values[i];
+//				for (int j=2; j<=(values.length/values[i]);j++) {
+//					if ((values[smallestNumber * j]) != 0) {
+//						values[smallestNumber * j] = 0;
+//						counter = counter + 1;
+//						if (counter == K) {
+//							System.out.println(values[smallestNumber * j]);
+//							break;
+//						}
+//					}
+//				}
+//			}
 //		}
-//		
+		int smallestNumber = 0;
+		// go through values
 		for (int i=0; i<values.length; i++) {
+			// check if smallest number is equal to zero
 			if (values[i] != 0) {
-				int smallestNumber = values[i];
-				for (int j=2; j<=(values.length/values[i]);j++) {
-					if ((smallestNumber * j) != 0) {
-						smallestNumber * j =
+				smallestNumber = values[i];
+				counter++;
+				if (counter == K) {
+					System.out.println(values[i]);
+				}
+				values[i] = 0;
+				
+				for (int j = smallestNumber + i; j<values.length; j = j + smallestNumber) {
+					if (values[j] != 0) {
+						counter++;	
+						if (counter == K) {
+							System.out.println(values[j]);
+						}
+						values[j] = 0;
 					}
 				}
 			}
+			
+			
+			
 		}
+		
+		
 		
 	}
 }
